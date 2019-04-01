5df8363e1e71d8ea4bec7736c1e7b38ee68402ac
MrTraner <juanmauricio.ochoa@hotmail.com>
2019-03-31 20:37:05 -0600

    Versión inicial

---
+++ ControlesPersonalizados/ControlesPersonalizados.Android/Renderers/CircularProgressRenderer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Xamarin.Forms.Platform.Android; //Para usar la clase ViewRenderer
+using com.refractored.monodroidtoolkit; //Para usar la clase HoloCircularProgressBar
+using System.ComponentModel; //Para usar la clase PropertyChangedEventArgs del método OnElementPropertyChanged
+using Xamarin.Forms; //Para usar la clase ExportRenderer
+using ControlesPersonalizados; //Para usar la clase CircularProgress
+using ControlesPersonalizados.Droid.Renderers; //Para usar la clase CircularProgressRenderer
+
+[assembly: ExportRenderer(typeof(CircularProgress), typeof(CircularProgressRenderer))]
+namespace ControlesPersonalizados.Droid.Renderers
+{
+    public class CircularProgressRenderer : ViewRenderer<CircularProgress, HoloCircularProgressBar>
+    {
+        public CircularProgressRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<CircularProgress> e)
+        {
+            base.OnElementChanged(e);
+
+            if(e.OldElement != null || this.Element == null)
+            {
+                return;
+            }
+
+            var progress = new HoloCircularProgressBar(Context)
+            {
+                Max = Element.Max,
+                Progress = Element.Progress,
+                Indeterminate = Element.Indeterminate,
+                ProgressColor = Element.ProgressColor.ToAndroid(),
+                ProgressBackgroundColor = Element.ProgressBackground.ToAndroid(),
+                IndeterminateInterval = Element.IndeterminateSpeed
+            };
+
+            SetNativeControl(progress);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if(Control == null || Element == null)
+            {
+                return;
+            }
+
+            if(e.PropertyName == CircularProgress.MaxProperty.PropertyName)
+            {
+                Control.Max = Element.Max;
+            }
+            else if (e.PropertyName == CircularProgress.ProgressProperty.PropertyName)
+            {
+                Control.Progress = Element.Progress;
+            }
+            else if (e.PropertyName == CircularProgress.IndeterminateProperty.PropertyName)
+            {
+                Control.Indeterminate = Element.Indeterminate;
+            }
+            else if (e.PropertyName == CircularProgress.ProgressColorProperty.PropertyName)
+            {
+                Control.ProgressColor = Element.ProgressColor.ToAndroid();
+            }
+            else if (e.PropertyName == CircularProgress.ProgressBackgroundProperty.PropertyName)
+            {
+                Control.ProgressBackgroundColor = Element.ProgressBackground.ToAndroid();
+            }
+            else if (e.PropertyName == CircularProgress.IndeterminateSpeedProperty.PropertyName)
+            {
+                Control.IndeterminateInterval = Element.IndeterminateSpeed;
+            }
+        }
+    }
+}
---
+++ ControlesPersonalizados/ControlesPersonalizados/CircularProgress.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms; //Para usar la clase View
+
+namespace ControlesPersonalizados
+{
+    public class CircularProgress : View
+    {
+        public static readonly BindableProperty ProgressProperty = BindableProperty.Create("Progress", typeof(float), typeof(CircularProgress), (float)0);
+
+        public float Progress
+        {
+            set
+            {
+                SetValue(ProgressProperty, value);
+            }
+            get
+            {
+                return (float)GetValue(ProgressProperty);
+            }
+        }
+
+        public static readonly BindableProperty MaxProperty = BindableProperty.Create("Max", typeof(float), typeof(CircularProgress), (float)0);
+
+        public float Max
+        {
+            set
+            {
+                SetValue(MaxProperty, value);
+            }
+            get
+            {
+                return (float)GetValue(MaxProperty);
+            }
+        }
+
+        public static readonly BindableProperty IndeterminateProperty = BindableProperty.Create("Indeterminate", typeof(bool), typeof(CircularProgress), false);
+
+        public bool Indeterminate
+        {
+            set
+            {
+                SetValue(IndeterminateProperty, value);
+            }
+            get
+            {
+                return (bool)GetValue(IndeterminateProperty);
+            }
+        }
+
+        public static readonly BindableProperty IndeterminateSpeedProperty = BindableProperty.Create("IndeterminateSpeed", typeof(int), typeof(CircularProgress), 0);
+
+        public int IndeterminateSpeed
+        {
+            set
+            {
+                SetValue(IndeterminateSpeedProperty, value);
+            }
+            get
+            {
+                return (int)GetValue(IndeterminateSpeedProperty);
+            }
+        }
+
+        public static readonly BindableProperty ProgressBackgroundProperty = BindableProperty.Create("ProgressBackground", typeof(Color), typeof(CircularProgress), Color.White);
+
+        public Color ProgressBackground
+        {
+            set
+            {
+                SetValue(ProgressBackgroundProperty, value);
+            }
+            get
+            {
+                return (Color)GetValue(ProgressBackgroundProperty);
+            }
+        }
+
+        public static readonly BindableProperty ProgressColorProperty = BindableProperty.Create("ProgressColor", typeof(Color), typeof(CircularProgress), Color.Red);
+
+        public Color ProgressColor
+        {
+            set
+            {
+                SetValue(ProgressColorProperty, value);
+            }
+            get
+            {
+                return (Color)GetValue(ProgressColorProperty);
+            }
+        }
+    }
+}
